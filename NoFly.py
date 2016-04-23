5fbdaf1be03ff1bd9b50cc7213d8e015950a43fe
mejo123 <stephenastf@gmail.com>
2016-04-23 13:12:01 -0400

    NoFly.py Bata 2.0

--- NoFly.py
+++ NoFly.py
@@ -1,22 +1,31 @@
 import math
 import Location
 
-def offlimit (lat, long, rad = 5, sscale = 0):
+def offlimit (lat, long, rad = 10, mph = 0):
+    count = 0
+    miles = 0
     with open("airports.dat", 'r') as f:
         for line in f:
             Name = line.split(',')[1]
-            latR = line.split(',')[6]
-            longR = line.split(',')[7]
-            try:
-                latR = int(float(latR))
-            except ValueError:
-                break
-            try:
-                longR = int(float(longR))
-            except ValueError:
-                   break
+          
+            for i in range(1,20):
+                latR = line.split(',')[i]
+                try:
+                    latR = float(latR)
+                    isfloat = True
+                except ValueError:
+                    isfloat = False
+                    continue
+                if isfloat:
+                    longR = line.split(',')[i+1]
+                    longR = float(longR)
+                    break
+            
+            count+=1
+            print(longR, latR)
             miles = findDis(lat, latR, long, longR)
-            if miles <= rad:
+            print(count, miles, latR, longR)
+            if miles <= rad + mph * .015:
                 return Name
     return 'good'
 
@@ -30,7 +39,7 @@ def dontfly (settings):
 def addData():
     pass
 
-def findDis(lat1, lat2, long1, long2):
+def findDis(lat1, lat2, long1, long2, count = 0 ):
     lat1 = math.radians(lat1)
     lat2 = math.radians(lat2)
     long1 = math.radians(long1)
@@ -39,7 +48,7 @@ def findDis(lat1, lat2, long1, long2):
     dlong = long1 - long2 
     a = pow(math.sin(dlat/2),2) + math.cos(lat1) * math.cos(lat2) * pow(math.sin(dlong/2),2)
     c =  2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    miles = c * 3961
+    miles = c * 3960
     return miles
     
 
