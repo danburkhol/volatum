f83d21fe49ba50f84bacd0c1a7792ddc21438be6
mejo123 <stephenastf@gmail.com>
2016-04-23 15:23:16 -0400

    NoFly Data Sync

--- NoFly.py
+++ NoFly.py
@@ -1,5 +1,8 @@
 import math
 import Location
+import imaplib
+import codecs
+
 
 def offlimit (lat, long, rad = 10, mph = 0):
     count = 0
@@ -37,27 +40,26 @@ def dontfly (settings):
         return False
 
 def addData():
-    pass
+    massiveList = []
+ 
+        #Take line
+    for line in open('airports.dat', 'r'):
+        L = []
+     
+        L = line.split(',')
+               
+        massiveList.append(L)
+        print(L)
 
-def findDis(lat1, lat2, long1, long2, count = 0 ):
-    lat1 = math.radians(lat1)
-    lat2 = math.radians(lat2)
-    long1 = math.radians(long1)
-    long2 = math.radians(long2)
-    dlat = lat1 - lat2
-    dlong = long1 - long2 
-    a = pow(math.sin(dlat/2),2) + math.cos(lat1) * math.cos(lat2) * pow(math.sin(dlong/2),2)
-    c =  2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    miles = c * 3960
     return miles
     
 
 if __name__ == '__main__':
-    coord = Location.getLocation()
-    #rad = int(input('rad: '))
-    offlimit = offlimit(coord[0], coord[1])
-    settings = True
-    if not offlimit == 'good':
-        dontfly(settings)
-        print(offlimit)
-    
+  #  coord = Location.getLocation()
+   # #rad = int(input('rad: '))
+    #offlimit = offlimit(coord[0], coord[1])
+    #settings = True
+    #if not offlimit == 'good':
+     #   dontfly(settings)
+      #  print(offlimit)
+    addData()
